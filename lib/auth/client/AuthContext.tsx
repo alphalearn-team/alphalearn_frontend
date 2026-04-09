65fb9e7c1d99235dfdebb0deaaad4fde12458c56
gabeywabey86B <gabrieljamesvalerio@gmail.com>
2026-04-09 12:42:32 +0800

    patch: fixed redirect for google auth

--- lib/auth/client/AuthContext.tsx
+++ lib/auth/client/AuthContext.tsx
@@ -175,9 +175,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
-        options: {
-            redirectTo: 'https://main.dri10xquta4qu.amplifyapp.com'
-        }
+        ...(callbackUrl ? { options: { redirectTo: callbackUrl } } : {}),
       })
 
       if (error) throw error;
